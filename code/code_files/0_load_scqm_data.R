feccdd0ebf008ed4a48db0c0f36111ef670e3f1d
dcourvoisier <38043567+dcourvoisier@users.noreply.github.com>
2024-03-30 11:15:38 +0100

    Update 0_load_scqm_data.R

--- code/code_files/0_load_scqm_data.R
+++ code/code_files/0_load_scqm_data.R
@@ -1,10 +1,10 @@
-# This will definitely not run on your end and does not contain anything interesting except the creation of the fvis object that just grabs each patients earliest visit
+# indicates which tables are used
 
 used_tables=c("patients","medications","radai5","haq","psada","euroqol","basfi","basdai","asas","sf_12","covid_19","mnyc_scoring","illness_mastering","socioeco","nsaids","steroids","health_issues","dlqi","diagnostic_image_sets","specimens","aliquots")
 
 con<-connect_rdb(snapshot = snapshot)
 
-#We always use 'loose' since 'strict' is very strict
+# 'loose' consent indicates that consent was given but may not be the latest version of the consent
 for (a in used_tables) {
   print(paste0("loading ",a))
   tab_a<-load_sql_table(a,con,consent = "loose")
@@ -12,9 +12,6 @@ for (a in used_tables) {
   rm(tab_a)
 }
 print(paste0("loading visits"))
-#requires special option
 visits<-load_sql_table("visits",con = con,consent = "loose",check_visits_for_phys_data = TRUE,to_visit_date = snapshot)
-
-#useful later
 
 fvis<-visits%>%group_by(patient_id)%>%slice_min(visit_date)%>%ungroup()%>%select(patient_id,visit_date)
